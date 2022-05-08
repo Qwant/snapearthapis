c83a0d2db3ae9343fc0bc7311d32387ccf57df85
Hicham Randrianarivo <hicham+github@randrianarivo.net>
2022-05-08 23:52:29 +0200

    Try with binary

--- tutorial.py
+++ tutorial.py
@@ -17,6 +17,8 @@
 !rm -rf snapearthapis
 !git clone https://github.com/chicham/snapearthapis.git
 !pip install -r snapearthapis/requirements.txt
+!apt-get -y install jq
+
 
 # %% [markdown]
 # # EarthSignature Tutorial
@@ -38,53 +40,21 @@ import base64
 import folium
 from shapely import wkt
 from shapely.geometry import mapping
-from grpclib.client import Channel
-from grpclib.config import Configuration
-from snapearth.api.v1.database_grpc import DatabaseProductServiceStub
-from snapearth.api.v1.database_pb2 import ListProductIdsRequest
-import nest_asyncio
-import asyncio
-
-nest_asyncio.apply()
 
 
 EUROPE_COORDINATES = wkt.loads(
     "POLYGON((-10.61 71.16, 44.85 71.16, 44.85 35.97, -10.61 35.97, -10.61 71.16))",
 )
 MAP_CENTER = mapping(EUROPE_COORDINATES.centroid)
-GRPC_CONFIGURATION = Configuration(
-        http2_connection_window_size=2**30,
-        http2_stream_window_size=2**30,
-)
-
 # %%
 
-# host = "earthsignature.snapearth.eu:443"
-host = "10.110.3.49:50051"
-ssl = False
+host = "earthsignature.snapearth.eu:443"
 
 map_ = folium.Map(
     location=MAP_CENTER["coordinates"][::-1],
     zoom_start=4,
     crs="EPSG3857",
 )
-
-async def get_responses(host, ssl):
-    async with Channel(host, ssl=ssl, configuration=GRPC_CONFIGURATION) as channel:
-        print(channel)
-
-
-    stub = DatabaseProductServiceStub(channel)
-    request = ListProductIdsRequest()
-    responses = stub.ListProductIds(request)
-    for response in responses:
-        geom = wkt.loads(response.wkt)
-        folium.GeoJson(geom).add_to(map_)
-        location = mapping(geom.centroid)["coordinates"][::-1]
-        folium.Marker(tooltip=f"{response.product_id}", location=location).add_to(
-            map_,
-        )
-map_ = asyncio.run(get_responses(host, ssl))
 
 # %% [markdown]
 # ## Query the API
